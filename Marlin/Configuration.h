97b25f5dcbab2c338be09efe9a1bb1bd7e8c4e60
Chmel <Stepan.Chmel@ataccama.com>
2021-12-17 19:26:16 +0100

    set z probe offset x77 y140

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -103,7 +103,7 @@
  * If you have a dual nozzle the offsets are calculated from the primary nozzle (the one plugged in to E0)
  */
 #define SENSOR_LEFT        0
-#define SENSOR_RIGHT       0
+#define SENSOR_RIGHT       23
 #define SENSOR_FRONT       0
 #define SENSOR_BEHIND      0
 
